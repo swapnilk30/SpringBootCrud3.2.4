68b907eceebdafd09dbee90a8f9eb0a013b01d8c
swapnilk30 <sk9137587676@gmail.com>
2024-07-07 20:50:30 +0530

    .

--- src/main/java/com/example/config/SwaggerConfig.java
+++ src/main/java/com/example/config/SwaggerConfig.java
@@ -1,10 +1,8 @@
 package com.example.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.w3c.dom.DocumentType;
 
-@Configuration
+
+//@Configuration
 public class SwaggerConfig {
 
 
